beeeb4e24582b0d4ad43ef00b6b1974794cdb696
samwlms <51151895+samwlms@users.noreply.github.com>
2021-04-23 13:26:18 +1000

    remove tail arg

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,7 @@ set -eax
 black --version
 
 # grab the info stored in the pre-commit-config file
-revision=$(grep -A3 'repos:' .pre-commit-config.yaml | tail -n1)
+revision=$(grep -A3 'repos:' .pre-commit-config.yaml)
 revision=${db//*rev: /}
-echo "$revision"
 
 black $@
