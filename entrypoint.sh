414c3396ba93ea16a804470e0fbe02859fed454d
samwlms <51151895+samwlms@users.noreply.github.com>
2021-04-23 13:32:31 +1000

    remove -A3 to show all lines

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -eax
 black --version
 
 # grab the info stored in the pre-commit-config file
-revision=$(grep -A3 'repos:' .pre-commit-config.yaml)
+revision=$(grep 'repos:' .pre-commit-config.yaml)
 revision=${db//*rev: /}
 
 black $@
