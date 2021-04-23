228327ec1c46b0ad32bfab9aebad80d2ab6087a8
samwlms <51151895+samwlms@users.noreply.github.com>
2021-04-23 16:39:14 +1000

    run verbose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 set -eax
 
+# create/ reinitialise git repo
 git init
-pre-commit install
-pre-commit run --all-files
+
+# uninstall existing hooks
+pre-commit uninstall
+
+# install hooks based on current configuration file
+pre-commit install -f
+
+# run pre-commit
+pre-commit run --all-files --verbose
+
+# sanity check for comparison
+black --check .
+
+
+
 
 # grab the info stored in the pre-commit-config file
 #prefix="language_version:"
 #language=$(grep $prefix .pre-commit-config.yaml | sed -e 's/^[ \t]*//')
 #output=${language#"$prefix"}
-
-
-
-
-black --check .
