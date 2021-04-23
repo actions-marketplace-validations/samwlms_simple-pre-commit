62170654a2b115a000c9ab5f6ac63c0ed7e94cfd
samwlms <51151895+samwlms@users.noreply.github.com>
2021-04-23 14:34:57 +1000

    group commands and operate on output '$()'

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ prefix="language_version:"
 # grab the line with pattern match for prefix var
 language_version=$(grep $prefix .pre-commit-config.yaml)
 # remove the leading whitespace
-language_version=$language_version | sed -e 's/^[ \t]*//'
+language_version=$($language_version | sed -e 's/^[ \t]*//')
 # trim prefix from line
 language_version=${language_version#"$prefix"} 
 
