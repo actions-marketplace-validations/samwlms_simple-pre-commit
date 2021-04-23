94c63704064504230d98e21b625486decd3b92d8
samwlms <51151895+samwlms@users.noreply.github.com>
2021-04-23 13:36:08 +1000

    tweaked var

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ black --version
 
 # grab the info stored in the pre-commit-config file
 revision=$(grep 'repos:' .pre-commit-config.yaml)
-grep_hooks = $(grep 'hooks:' revision)
+grep_hooks = $(grep 'hooks:' .pre-commit-config.yaml)
 hooks_var=${revision//*hooks: /}
 
 black $@
