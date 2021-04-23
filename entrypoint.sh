d7e1c8ab9f426abe8a485daad0edd29521791825
samwlms <51151895+samwlms@users.noreply.github.com>
2021-04-23 14:23:56 +1000

    update with comments, use same var

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,15 @@ set -eax
 black --version
 
 # grab the info stored in the pre-commit-config file
-prefix='language_version:'
-language_version=$(grep 'language_version:' .pre-commit-config.yaml)
-output=${language_version#"$prefix"}
 
-echo $output
+# the prefix (aka yaml key) to match to
+prefix="language_version:"
+# grab the line with pattern match for prefix var
+language_version=$(grep $prefix .pre-commit-config.yaml)
+# trim prefix from line
+language_version=${language_version#"$prefix"} 
+
+echo "$language_version"
 
 
 black $@
