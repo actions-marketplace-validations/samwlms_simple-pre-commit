0c4d791a74836cb15a15790205007bea2224ce55
samwlms <51151895+samwlms@users.noreply.github.com>
2021-04-23 14:52:41 +1000

    remove clutter - simpler tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,18 +5,10 @@ black --version
 
 # grab the info stored in the pre-commit-config file
 
-grep "language_version:" .pre-commit-config.yaml | sed -e 's/^[ \t]*//'
+language=$(grep "language_version:" .pre-commit-config.yaml | sed -e 's/^[ \t]*//')
+echo "$language"
 
-# the prefix (aka yaml key) to match to
-prefix="language_version:"
-# grab the line with pattern match for prefix var
-language_version=$(grep $prefix .pre-commit-config.yaml)
-# remove the leading whitespace
-language_version=$($language_version sed -e 's/^[ \t]*//')
-# trim prefix from line
-language_version=${language_version#"$prefix"} 
 
-echo "$language_version"
 
 
 black $@
