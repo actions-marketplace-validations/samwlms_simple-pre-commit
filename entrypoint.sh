c19e2c0eb823e6480433d43a89c860fc32f8dcda
samwlms <51151895+samwlms@users.noreply.github.com>
2021-04-23 15:08:52 +1000

    try with just pre-commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,15 @@
 set -eax
 
 black --version
+pre-commit install
+pre-commit run --all-files
 
 # grab the info stored in the pre-commit-config file
-prefix="language_version:"
-language=$(grep $prefix .pre-commit-config.yaml | sed -e 's/^[ \t]*//')
-output=${language#"$prefix"}
+#prefix="language_version:"
+#language=$(grep $prefix .pre-commit-config.yaml | sed -e 's/^[ \t]*//')
+#output=${language#"$prefix"}
 
 
 
 
-black $@
+#black $@
