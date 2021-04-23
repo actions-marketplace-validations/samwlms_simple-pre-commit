e9d81784bb2fa295a089597ea89b061207597e92
samwlms <51151895+samwlms@users.noreply.github.com>
2021-04-23 14:02:10 +1000

    include 'tr' in var instansiation

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -eax
 black --version
 
 # grab the info stored in the pre-commit-config file
-language_version=$(grep 'language_version:' .pre-commit-config.yaml) | tr -d 'language_version:'
+language_version=$(grep 'language_version:' .pre-commit-config.yaml | tr -d 'language_version:')
 
 echo $language_version
 
