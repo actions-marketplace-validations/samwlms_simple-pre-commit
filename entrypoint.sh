b40c5127cbd5553ceaafbe3f1f99387d245f6e8a
samwlms <51151895+samwlms@users.noreply.github.com>
2021-04-23 13:43:54 +1000

    more testing with grep vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,12 @@ set -eax
 black --version
 
 # grab the info stored in the pre-commit-config file
-revision=$(grep 'repos:' .pre-commit-config.yaml)
-grep_hooks = $(grep 'hooks:' .pre-commit-config.yaml)
+echo ===== Grep testing ======
+echo === test1 ===
+revision=$(grep -A 10 'repos:' .pre-commit-config.yaml)
+echo === test2 ===
+grep_hooks=$(grep -A 10 'hooks:' .pre-commit-config.yaml)
+echo === test3 ===
 hooks_var=${revision//*hooks: /}
 
 black $@
